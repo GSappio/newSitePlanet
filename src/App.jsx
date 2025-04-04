dd64aad85d2531aa3329c52f0d9289426d0c9363
Bielsappio <gabriel.sappio02@gmail.com>
2025-04-04 15:20:40 -0300

    fix project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import SobreNos from './pages/About'
-import Menu from './pages/Menu'
-import Delivery from './pages/Delivery'
+  import React from 'react';
+  import { Routes, Route, BrowserRouter } from 'react-router-dom';
+  import Home from './pages/Home';
+  import SobreNos from './pages/About'
+  import Menu from './pages/Menu'
+  import Delivery from './pages/Delivery'
 
 
-function App() {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/sobre" element={<SobreNos />} />
-        <Route path="/entrega" element={<Delivery />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+  function App() {
+    return (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/sobre" element={<SobreNos />} />
+          <Route path="/entrega" element={<Delivery />} />
+        </Routes>
+    );
+  }
 
-export default App;
+  export default App;
